69299c6592ad15523469ba54adeda12b55b8f415
BergNovis <jacktwist.ennis@outlook.com>
2020-04-29 13:30:36 +0800

    将本地代码托管至github

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>this is App</div>
+</template>    
+
+<script>
+import img from './assets/images/bg.jpg'
+
+import './assets/images/Checked.svg'
+
+import './assets/styles/test.css'
+
+export default {
+    name: 'App',
+    created() {
+        console.log(img);
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// 创建Vue根实例
+import Vue from 'vue'
+
+// 导入App组件
+import App from './App.vue'
+
+// 创建Vue根实例
+new Vue({
+    el: '#app',
+    components: {
+        // 组件名：组件对象，前后相同时可以简写成App
+        // App: App,
+        App
+    },
+    template: '<App/>'
+})
+// 挂载App组件
+
